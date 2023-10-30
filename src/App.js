dc3c92989db4b8666c2ab64bdade739938867e5d
Reshav ji <116823969+Reshavji@users.noreply.github.com>
2023-10-30 16:51:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+ 
+
+  return (
+    <div className="app">
+      <div className="app__top"></div>
+      <div className="app__container">
+       <h1>Home</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
