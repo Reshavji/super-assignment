821ed1051d42f508339aee0040d9e74d2141cc2e
Reshav ji <116823969+Reshavji@users.noreply.github.com>
2023-10-30 16:53:57 +0530

    Delete Backend directory

--- Backend/routes/event.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const router = new express.Router();
-const eventController = require('../controllers/event');
-router.post('/create', eventController.createEvent);
-module.exports = router;
--- Backend/routes/user.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = new express.Router();
-const userController = require('../controllers/user.js');
-router.post('/register', userController.registerUser);
-router.post("/login", userController.loginUser);
-module.exports = router;
